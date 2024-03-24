227f706908faa0aad8b2e1927c42cf6c65110af9
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-03-24 20:08:40 -0300

    Iniciado config de log

---
+++ CatalogApi/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace CatalogApi.Logging;
+
+public class CustomLoggerProvider(CustomLoggerProviderConfiguration loggerConfig) : ILoggerProvider
+{
+    private readonly ConcurrentDictionary<string, CustomerLogger> _loggers = new();
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return _loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerConfig));
+    }
+    
+    public void Dispose()
+    {
+        _loggers.Clear();
+    }
+}
---
+++ CatalogApi/Logging/CustomerLogger.cs
@@ -0,0 +1,46 @@
+﻿namespace CatalogApi.Logging;
+
+public class CustomerLogger : ILogger
+{
+    private readonly CustomLoggerProviderConfiguration _loggerConfig;
+    private readonly string _loggerName;
+
+    public CustomerLogger(string name, CustomLoggerProviderConfiguration loggerConfig)
+    {
+        _loggerName = name;
+        _loggerConfig = loggerConfig;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == _loggerConfig.LogLevel;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        var message = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+        SaveLogFile(message);
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return null;
+    }
+
+    public void SaveLogFile(string message)
+    {
+        const string path = @"C:\dev\CatalogApi\CatalogApi\Logging\log.txt";
+        using var streamWriter = new StreamWriter(path, true);
+        try
+        {
+            streamWriter.WriteLine(message);
+            streamWriter.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
