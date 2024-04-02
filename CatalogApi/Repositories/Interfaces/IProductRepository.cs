2f9b36afed37ee1b742a3648320af586d508f942
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-04-01 21:46:57 -0300

    Iniciado repository para product

---
+++ CatalogApi/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using CatalogApi.Models;
+
+namespace CatalogApi.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    public Task<IQueryable<Product>> GetProductsAsync();
+    public Task<Product?> GetProductAsync(int id);
+    public Task<Product> CreateAsync(Product product);
+    public Task<bool> UpdateAsync(Product product);
+    public Task<bool> DeleteAsync(int id);
+}
